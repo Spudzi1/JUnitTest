1e420e7c58914a55a02c19983726af9454d31358
Jakob <jakob.storm@live.dk>
2024-10-01 12:30:46 +0200

    Lille dagligskæv opdatering Controller begyndelse til dagligskæv opdatering i dosis

--- src/ordination/Dosis.java
+++ src/ordination/Dosis.java
@@ -5,11 +5,16 @@ import java.time.LocalTime;
 public class Dosis {
     private final LocalTime tid;
     private final double antal;
+    private final DagligSkæv dagligSkæv;
+    private final DagligFast dagligFast;
 
 
-    public Dosis(LocalTime tid, double antal ) {
+    /** Note: DagligSkæv og DagligFast is nullable */
+    public Dosis(LocalTime tid, double antal, DagligSkæv dagligSkæv, DagligFast dagligFast) {
         this.tid = tid;
         this.antal = antal;
+        this.dagligSkæv = dagligSkæv;
+        this.dagligFast = dagligFast;
     }
 
     public double getAntal() {
