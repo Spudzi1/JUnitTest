e288b6e37742760118586ba2bc2545eddf1d6f27
Jakob <jakob.storm@live.dk>
2024-10-01 11:35:40 +0200

    DagligSkæv opdatering + Tilføjelse Ordination

--- src/ordination/Ordination.java
+++ src/ordination/Ordination.java
@@ -1,11 +1,14 @@
 package ordination;
 
+import com.sun.source.tree.ReturnTree;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public abstract class Ordination {
     private final LocalDate startDato;
     private final LocalDate slutDato;
+    private Lægemiddel lægemiddel;
 
     protected Ordination(LocalDate startDato, LocalDate slutDato) {
         this.startDato = startDato;
@@ -41,4 +44,15 @@ public abstract class Ordination {
 
     /** Returner ordinationstypen som en String. */
     public abstract String getType();
+
+    /** Returner Lægemidlet som en string
+     * Note: Nullable return value. */
+    public String getLægemiddel() {
+        return lægemiddel.toString();
+    }
+
+    /** Note: Nullable param lægemiddel */
+    public void setLægemiddel(Lægemiddel lægemiddel) {
+        this.lægemiddel = lægemiddel;
+    }
 }
