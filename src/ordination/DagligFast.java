fee9c9562d55985baef1c859cd259715d3ec30a5
JosephineGormsen <josephinegormsen@hotmail.com>
2024-10-01 11:02:23 +0200

    Færdiggjort DagligFast og tilføjet i Dosis

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,10 @@
 package ordination;
 
 public class DagligFast {
+    private final Dosis[] doser = new Dosis[4];
+
+    public Dosis[] getDoser() {
+        return doser;
+    }
 
 }
--- src/ordination/Dosis.java
+++ src/ordination/Dosis.java
@@ -5,10 +5,14 @@ import java.time.LocalTime;
 public class Dosis {
     private final LocalTime tid;
     private final double antal;
+    private final DagligFast dagligFast;
 
-    public Dosis(LocalTime tid, double antal) {
+
+    public Dosis(LocalTime tid, double antal, DagligFast dagligFast ) {
         this.tid = tid;
         this.antal = antal;
+        this.dagligFast = dagligFast;
+
     }
 
     public double getAntal() {
