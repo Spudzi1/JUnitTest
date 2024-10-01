7b65789f203780615120c165c89cd6650c5d5e44
JosephineGormsen <josephinegormsen@hotmail.com>
2024-10-01 11:18:02 +0200

    Færdiggjort DagligFast og tilføjet i Dosis pt 2

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,10 +1,37 @@
 package ordination;
 
-public class DagligFast {
+import java.time.LocalDate;
+
+public class DagligFast extends Ordination {
     private final Dosis[] doser = new Dosis[4];
+
+    public DagligFast(LocalDate startDato, LocalDate slutDato){
+        super(startDato, slutDato);
+    }
 
     public Dosis[] getDoser() {
         return doser;
+    }
+
+    @Override
+    public double samletDosis(){
+        double totalDosis = 0;
+        for (Dosis dosis : doser) {
+            if (dosis != null) {
+                totalDosis += dosis.getAntal();
+            }
+        }
+        return totalDosis;
+    }
+
+    @Override
+    public double døgnDosis(){
+        return samletDosis() / antalDage();
+    }
+
+    @Override
+    public String getType(){
+        return "Daglig fast";
     }
 
 }
