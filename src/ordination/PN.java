3eb8fcc3b16cc488ef4c79e028327b9df4238955
JosephineGormsen <josephinegormsen@hotmail.com>
2024-10-01 11:43:58 +0200

    Færdiggjort PN

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -1,9 +1,18 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PN {
+public class PN extends Ordination {
+
     private final double antalEnheder;
+    private final List<LocalDate> anvendteDoser = new ArrayList<>();
+
+   public PN(LocalDate startDato, LocalDate slutDato, double antalEnheder){
+        super(startDato, slutDato);
+        this.antalEnheder = antalEnheder;
+   }
 
     public double getAntalEnheder() {
         return antalEnheder;
@@ -13,12 +22,30 @@ public class PN {
      * Registrer datoen for en anvendt dosis.
      */
     public void anvendDosis(LocalDate dato) {
-
+        if ((dato.isEqual(getStartDato()) || dato.isAfter(getStartDato())) &&
+                (dato.isEqual(getSlutDato()) || dato.isBefore(getSlutDato()))) {
+            anvendteDoser.add(dato);
+        } else {
+            throw new IllegalArgumentException("Datoen ligger uden for ordinationens start- og slutdato.");
+        }
     }
 
     /** Returner antal gange ordinationen er anvendt. */
     public int antalGangeAnvendt() {
+        return anvendteDoser.size();
+    }
 
-        return -1;
+    @Override
+    public double samletDosis(){
+        return antalEnheder * antalGangeAnvendt();
+    }
+
+    public double døgnDosis(){
+        return samletDosis() / antalDage();
+    }
+
+    @Override
+    public String getType(){
+        return "PN: ";
     }
 }
