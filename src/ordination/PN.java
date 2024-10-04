f14814d6fd84972b5731523fe6b27f23fdf0f924
Nikolai <eaa23nlw@students.eaaa.dk>
2024-10-04 09:28:47 +0200

    PN samlet dosis update

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -1,7 +1,5 @@
 package ordination;
 
-import controller.Controller;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,24 +9,24 @@ public class PN extends Ordination {
     private final double antalEnheder;
     private final List<LocalDate> anvendteDoser = new ArrayList<>();
 
-   public PN(LocalDate startDato, LocalDate slutDato, double antalEnheder, Lægemiddel lægemiddel){
+    public PN(LocalDate startDato, LocalDate slutDato, double antalEnheder, Lægemiddel lægemiddel) {
         super(startDato, slutDato, lægemiddel);
         this.antalEnheder = antalEnheder;
-   }
+    }
 
     public double getAntalEnheder() {
         return antalEnheder;
     }
 
-    public List<LocalDate> getAnvendteDoser(){
+    public List<LocalDate> getAnvendteDoser() {
         return new ArrayList<>(anvendteDoser);
     }
 
-    public void addAnvendtDosis(LocalDate dato){
+    public void addAnvendtDosis(LocalDate dato) {
         anvendteDoser.add(dato);
     }
 
-    public void removeAnvendtDosis(LocalDate dato){
+    public void removeAnvendtDosis(LocalDate dato) {
         anvendteDoser.remove(dato);
     }
 
@@ -39,23 +37,29 @@ public class PN extends Ordination {
         anvendteDoser.add(dato);
     }
 
-    /** Returner antal gange ordinationen er anvendt. */
+    /**
+     * Returner antal gange ordinationen er anvendt.
+     */
     public int antalGangeAnvendt() {
         return anvendteDoser.size();
     }
 
     @Override
-    public double samletDosis(){
-        return antalEnheder * antalGangeAnvendt();
+    public double samletDosis() {
+        double samletDosis = 0;
+        for (LocalDate date : anvendteDoser) {
+            samletDosis += antalEnheder;
+        }
+        return samletDosis;
     }
 
     @Override
-    public double døgnDosis(){
+    public double døgnDosis() {
         return samletDosis() / antalDage();
     }
 
     @Override
-    public String getType(){
+    public String getType() {
         return "PN: ";
     }
 
