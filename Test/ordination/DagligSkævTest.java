46741ae9999086ed24b1026b2537c442b7f601ba
Jakob <jakob.storm@live.dk>
2024-10-04 09:17:54 +0200

    Test af ConstructDagligSkæv

--- Test/ordination/DagligSkævTest.java
+++ Test/ordination/DagligSkævTest.java
@@ -3,12 +3,37 @@ package ordination;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DagligSkævTest {
 
+    @Test
+    void ConstructDagligSkæv() {
+        //Arrange
+        Lægemiddel L1 = new Lægemiddel("Acetylsalicylsyre", 0.1, 0.15, 0.16, "Styk");
+
+        //Act
+        DagligSkæv DagligSkæv1 = new DagligSkæv(LocalDate.of(2024, 9, 10), LocalDate.of(2024, 9, 10), L1);
+
+        //Assert
+        assertEquals(LocalDate.of(2024,9,10), DagligSkæv1.getStartDato());
+        assertEquals(LocalDate.of(2024,9,10), DagligSkæv1.getSlutDato());
+        assertEquals(L1, DagligSkæv1.getLægemiddel());
+        assertEquals(new ArrayList<Dosis>(), DagligSkæv1.getDoser());
+    }
+
+    @Test
+    void samletDosis() {
+        //Arrange
+        Lægemiddel L1 = new Lægemiddel("Acetylsalicylsyre", 0.1, 0.15, 0.16, "Styk");
 
 
+    }
+
+    @Test
+    void døgnDosis() {
+    }
 }
